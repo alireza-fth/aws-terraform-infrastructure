a79699b86fef47ca5eeeef4881c8ca80fbb1a656
Alireza Fathihafshejani <311056228+alireza-fth@users.noreply.github.com>
2026-07-30 17:00:53 +0200

    Create web security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,3 +45,38 @@ resource "aws_route_table_association" "public" {
 
   route_table_id = aws_route_table.public.id
 }
+resource "aws_security_group" "web" {
+  name = "web-security-group"
+
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-security-group"
+  }
+}
