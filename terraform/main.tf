10baca7cfd49b73ea34d86691eb1689710d88b85
Alireza Fathihafshejani <311056228+alireza-fth@users.noreply.github.com>
2026-07-30 16:55:50 +0200

    Configure public route table

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,40 @@ resource "aws_vpc" "main" {
     Name = "cloud-engineering-vpc"
   }
 }
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.1.0/24"
+
+  availability_zone = "eu-central-1a"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-internet-gateway"
+  }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+
+  route_table_id = aws_route_table.public.id
+}
